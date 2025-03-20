4f29ef1a2cc666e9cb9ef3fbd54c48aa8be2ab48
Natalia Krzysztoporska <s32521@pjwstk.edu.pl>
2025-03-20 15:17:53 +0100

    zadanie

---
+++ Student.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    String imie;
+    String nazwisko;
+    String nIndeksu;
+    String email;
+    List<Double> oceny = new ArrayList<>();
+
+    public Student(String imie, String nazwisko, String nIndeksu, String email) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.nIndeksu = nIndeksu;
+        this.email = email;
+    }
+    public void oceny(double oceny) {
+
+    }
+
+}
---
+++ StudentGroup.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentGroup {
+        public String nazwa;
+        List<Student> students = new ArrayList<>();
+
+        public StudentGroup(String nazwa) {
+            this.nazwa = nazwa;
+        }
+        public void addStudent(Student student) {
+
+        }
+    }
+
